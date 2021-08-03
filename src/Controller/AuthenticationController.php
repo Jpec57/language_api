86286d52dc2dc0e81579a0f6370ecd1fecf7918c
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2021-08-03 21:37:16 +0200

    Renaming

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -12,9 +12,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class AuthenticationController extends AbstractController
 {
